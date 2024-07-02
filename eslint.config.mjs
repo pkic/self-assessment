2fe7ec53822eca857cce5db15330cda98dd4090c
3keyroman <roman.cinkais@3key.company>
2024-07-02 17:49:34 +0200

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,32 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import github from 'eslint-plugin-github';
-import typescriptParser from '@typescript-eslint/parser';
-import path from 'path';
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import github from "eslint-plugin-github";
+import typescriptParser from "@typescript-eslint/parser";
+import path from "path";
 
 export default [
-    {
-        ignores: ["dist/"],
+  {
+    ignores: ["dist/"],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      github,
     },
-    {
-        files: ["**/*.ts", "**/*.tsx"],
-        plugins: {
-            '@typescript-eslint': typescriptEslint,
-            github,
-        },
-        languageOptions: {
-            parser: typescriptParser,
-            ecmaVersion: 2020,
-            sourceType: 'module',
-            parserOptions: {
-                project: path.resolve('./tsconfig.json'),
-            },
-            globals: {
-                es2020: true,
-            }
-        },
-        rules: {
-            semi: "error",
-            "prefer-const": "error"
-        },
-    }
+    languageOptions: {
+      parser: typescriptParser,
+      ecmaVersion: 2020,
+      sourceType: "module",
+      parserOptions: {
+        project: path.resolve("./tsconfig.json"),
+      },
+      globals: {
+        es2020: true,
+      },
+    },
+    rules: {
+      semi: "error",
+      "prefer-const": "error",
+    },
+  },
 ];
